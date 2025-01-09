8da8ec4c97d32674bdbe27cd0acbbbc9e7564fe8
lazarstanozevic0915 <lstanojevicand@gmail.com>
2025-01-09 09:19:56 +0000

    fix: update price stripe key

--- components/stripe/StripeTable.tsx
+++ components/stripe/StripeTable.tsx
@@ -1,8 +1,9 @@
-'use client'
+'use client';
 import { User } from '@supabase/supabase-js';
 import React, { useEffect } from 'react';
 
-interface StripePricingTableProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> {
+interface StripePricingTableProps
+  extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> {
   'pricing-table-id': string;
   'publishable-key': string;
 }
@@ -17,32 +18,31 @@ declare global {
 
 type Props = {
   user: User;
-}
+};
 
 const StripePricingTable = ({ user }: Props) => {
   useEffect(() => {
     const script = document.createElement('script');
-    script.src = "https://js.stripe.com/v3/pricing-table.js";
+    script.src = 'https://js.stripe.com/v3/pricing-table.js';
     script.async = true;
 
     document.body.appendChild(script);
 
     return () => {
       document.body.removeChild(script);
-    }
+    };
   }, []);
 
   return (
     <div className='flex flex-1 flex-col w-full'>
       <stripe-pricing-table
-          pricing-table-id="prctbl_1P0TL0C3ic5Sd20TGpWOU2Fi"
-          publishable-key="pk_live_51P0SikC3ic5Sd20T9QRaRKIkqy8l951LDgeOxcP24ZRXHnQzjnOFM7tfhsYdWksn1wNBdejJzvaxXGq0yRAxm14A00Py0XreGk"
-          client-reference-id={user.id}
-          customer-email={user.email}
-      >
-      </stripe-pricing-table>
+        pricing-table-id='prctbl_1QfEy6GgmRxtOJ6gN46KR14R'
+        publishable-key='pk_test_51Qf6YDGgmRxtOJ6gQzvyBjmlM191p6cWStvFWzw3KPe3vhM9O4Do3rrmhaHgVNnP76IhpxIlnk5WdxGeXZoacnVJ00RkplzDGg'
+        client-reference-id={user.id}
+        customer-email={user.email}
+      ></stripe-pricing-table>
     </div>
   );
-}
+};
 
 export default StripePricingTable;
